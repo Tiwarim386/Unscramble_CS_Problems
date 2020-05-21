bec2d6ba65f1a9ad8be1e40f1bb7124379d0eab9
Mohit Tiwari <17ucs092@lnmiit.ac.in>
2020-05-21 16:00:34 +0530

    Implemented the solution
    According to the instructions provided

--- Task0.py
+++ Task0.py
@@ -15,9 +15,11 @@ with open('calls.csv', 'r') as f:
 """
 TASK 0:
 What is the first record of texts and what is the last record of calls?
+
 Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
+
 """
 print("First record of texts, {} texts {} at time {}"
       .format(texts[0][0], texts[0][1], texts[0][2]))
